ad700f544d2f2cc2b044ce86dc372cfd725c2fd2
Kasun Sri Buddika Fonseka <131034543+kasunsri@users.noreply.github.com>
2024-12-09 20:42:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDo from './compornents/ToDo'
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4 h-screen'>
+      <ToDo/>
+    </div>
+  )
+}
+
+export default App
